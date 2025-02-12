9e6ffcaf6db0c1e345e9db630779a229a5cb2e12
Takeda Sa-sachio <xingshengwutian@gmail.com>
2025-02-12 15:39:01 +0900

    Initial commit

---
+++ src/main/java/raisetech/StudentManagement/StudentRepository.java
@@ -0,0 +1,16 @@
+package raisetech.StudentManagement;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface StudentRepository {
+
+  @Select("SELECT * FROM students")
+  List<Student> search();
+
+  @Select("SELECT * FROM students_courses")
+  List<StudentsCourses> searchStudentsCourses();
+}
+
