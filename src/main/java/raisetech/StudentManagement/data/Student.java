24f4fc2168c6a49f654b068b49c07ad07d41a741
Takeda Sa-sachio <xingshengwutian@gmail.com>
2025-10-29 15:38:39 +0900

    Merge remote-tracking branch 'origin/main'

--- src/main/java/raisetech/StudentManagement/data/Student.java
+++ src/main/java/raisetech/StudentManagement/data/Student.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 import org.apache.ibatis.annotations.Result;
@@ -16,6 +17,7 @@ import org.apache.ibatis.annotations.Select;
 public class Student {
 
   @NotBlank
+  @Pattern(regexp = "^\\d+$", message = "数字のみ入力するようにしてください。")
   private String id;
 
   @NotBlank
